bc9ed46202029403bb9e8a667b60989543241786
krish-kolarya <krishnakolarya@gmail.com>
2025-08-02 13:28:41 +0530

    Fix axios base URL to include '/api' for production environment

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 const BACKEND_URI = import.meta.env.VITE_BACKEND_API_URL
-const BASE_URL = import.meta.env.MODE === "development" ? "http://localhost:5001/api" : BACKEND_URI;
+const BASE_URL = import.meta.env.MODE === "development" ? "http://localhost:5001/api" : `${BACKEND_URI}/api`;
 
 export const axiosInstance = axios.create({
   baseURL: BASE_URL,
