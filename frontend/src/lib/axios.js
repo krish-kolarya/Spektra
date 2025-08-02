c18111059fbe48759284ab88200b73c7fd459df7
krish-kolarya <krishnakolarya@gmail.com>
2025-08-02 13:02:22 +0530

    Update axios configuration to use dynamic backend URI

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
-
-const BASE_URL = import.meta.env.MODE === "development" ? "http://localhost:5001/api" : "/api";
+const BACKEND_URI = import.meta.env.VITE_BACKEND_API_URL
+const BASE_URL = import.meta.env.MODE === "development" ? "http://localhost:5001/api" : BACKEND_URI;
 
 export const axiosInstance = axios.create({
   baseURL: BASE_URL,
